914e759b428131fca6f1bca069b2f62df7ae6126
Oded <oatias@baybridgedigital.com>
2024-02-22 22:53:43 +0200

    TSK-5: Adding header to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Colors } from './style';
+import { Colors, Header } from './style';
 
 
 const App: React.FC = () => {
   return (
-    <div style={{height : '100vh', width: '100%', backgroundColor : Colors?.brandPrimaryBackgroundColor, margin : 0, padding : 0}}>
-      <h1 style = {{color: Colors?.brandPrimaryTextColor , margin : 0}}>Listify</h1>
-    </div>
+    <>
+      <div style={{ height: '100vh', width: '100%', backgroundColor: Colors?.brandPrimaryBackgroundColor, margin: 0, padding: 0 }}>
+        <Header height={50}/>
+        <h1 style={{ color: Colors?.brandPrimaryTextColor, margin: 0 }}>Listify</h1>
+      </div>
+    </>
   );
 }
 
---
+++ src/style/theme/StyledComponents.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+import { Colors } from '.';
+
+interface Header {
+    backgroundColor?: string,
+    height ?: number,
+
+}
+
+export const Header = styled.header<Header>`
+  background-color: ${(props: any) => (props.backgroundColor ? props.backgroundColor : Colors?.brandSecondaryBackgroundColor)};
+  height : ${(props: any) => (props.height ? `${props.height}px` : '20px')}
+  `;
+
